9064fa87fd30116e8b87e8983da154d161599746
jojo0094 <aungkyawkyaw0094@gmail.com>
2025-02-15 00:39:03 +1300

    1) a dedicated endpoint (where I have learned simlar feature like ruby fitler list or python list comprehensio for book id match) and 2) it is then being trigerred by redirect in post /books

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import createHomepageTemplate from './views/index.js';
 import createListTemplate from './views/list.js';
 import BOOKS_DATA from './data/data.js';
-//import createBookTemplate from './views/book.js';
+import createBookTemplate from './views/book.js';
 
 // create app
 const app = express();
@@ -25,9 +25,17 @@ app.post('/books', (req, res) => {
   const id = Math.random().toString();
 
   BOOKS_DATA.push({id, title, author});
+  
+  res.redirect(`/books/${id}`);
 
-  res.send(`<li>${title}, ${author}</li>`)
 });
+
+//to do a dedicated endpoint for a single book
+app.get('/books/:id', (req, res) => {
+  const {id} = req.params;
+  const book = BOOKS_DATA.find((book) => book.id === id); //look like a ruby filter or python list comprehension filter.
+  res.send(createBookTemplate(book));
+})
 
 // listen to port
 app.listen(3000, () => {
